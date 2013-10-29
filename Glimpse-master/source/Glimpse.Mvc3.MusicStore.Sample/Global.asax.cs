33614cc92a687cb5253897dfdf4632d10bcaa4ea
cicimen <egemengulkilik@gmail.com>
2013-10-29 02:50:48 +0200

    adfs
    asd

---
+++ Glimpse-master/source/Glimpse.Mvc3.MusicStore.Sample/Global.asax.cs
@@ -0,0 +1,91 @@
+﻿using MvcMusicStore.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Dapper;
+
+namespace MvcMusicStore
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var myRoute = new Route("Blog/Archive/{year}/{month}/{day}", new RouteValueDictionary { { "controller", "Blog" }, { "action", "archive" }, { "year", "" }, { "month", "" },  { "day", "" }, { "page", 0 } }, null, new RouteValueDictionary { { "Test", "Data" }, { "Other", "Me" } }, new MvcRouteHandler());
+            
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.Add(myRoute);
+
+            routes.MapRoute(
+                "BlogArchive",
+                "Archive/{entryDate}",
+                new { controller = "Blog", action = "Archive" },
+                new { entryDate = @"d{2}-d{2}-d{4}" }
+            );
+
+            routes.MapRoute(
+                "Product",
+                "Product/Insert",
+                new { controller = "Product", action = "Insert" },
+                new { httpMethod = new HttpMethodConstraint("POST"), random = "Test" }
+            );
+
+            routes.MapRoute(
+                "OtherDefaultBrowse", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Browse", id = UrlParameter.Optional }, // Parameter defaults
+                new { controller = "Home", action = @"Test" }
+            );
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+            routes.MapRoute(
+                "DefaultTest", // Route name
+                "{controller}/{action}/{id}/{test}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional, test = 1 }, // Parameter defaults
+                new { action = @"Index", controller = "Test" }
+            ); 
+        }
+
+        private void LoadConfiguration()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["MusicStoreEntities"];
+            var factory = DbProviderFactories.GetFactory(connectionString.ProviderName);
+            using (var connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = connectionString.ConnectionString;
+                connection.Open();
+
+                MvcMusicStore.Framework.Configuration.Current = connection.Query<ConfigurationModel>("SELECT * FROM Configuration").First(); 
+            }
+        }
+
+        protected void Application_Start()
+        {
+            System.Data.Entity.Database.SetInitializer(new MvcMusicStore.Models.SampleData());
+
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+            LoadConfiguration();
+        }
+    }
+}
---
+++ Glimpse-master/source/Glimpse.Test.AspNet/Tab/RoutesShould.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using Glimpse.AspNet.AlternateType;
+using Glimpse.AspNet.Model;
+using Glimpse.AspNet.Tab;
+using Glimpse.Core.Extensibility;
+using Glimpse.Core.Message;
+using Glimpse.Test.Common; 
+using Moq;
+using Xunit;
+using Xunit.Extensions; 
+
+namespace Glimpse.Test.AspNet.Tab
+{ 
+    public class RoutesShould
+    {
+        [Theory, AutoMock]
+        public void ReturnName(Routes tab)
+        {
+            Assert.Equal("Routes", tab.Name);
+        }
+
+        [Theory, AutoMock]
+        public void ReturnDocumentationUri(Routes tab)
+        {
+            Assert.True(tab.DocumentationUri.Contains("getGlimpse.com"));
+        }
+
+        [Theory, AutoMock]
+        public void ReturnRouteInstancesEvenWhenContextIsNull(Routes tab, ITabContext context)
+        {
+            context.Setup(x => x.GetRequestContext<HttpContextBase>()).Returns((HttpContextBase)null);
+
+            System.Web.Routing.RouteTable.Routes.Clear();
+            System.Web.Routing.RouteTable.Routes.Ignore("Test");
+             
+            var data = tab.GetData(context) as IList<RouteModel>;
+
+            Assert.NotNull(data);
+            Assert.Equal(System.Web.Routing.RouteTable.Routes.Count, data.Count); 
+        }
+
+        [Theory, AutoMock]
+        public void ReturnRouteInstancesEvenWhenRoutesTableEmpty(Routes tab, ITabContext context)
+        {
+            System.Web.Routing.RouteTable.Routes.Clear();
+
+            var data = tab.GetData(context) as IList<RouteModel>;
+
+            Assert.NotNull(data);
+            Assert.Empty(data);
+        }
+
+        [Theory, AutoMock]
+        public void ReturnProperNumberOfInstances(Routes tab, ITabContext context)
+        {
+            System.Web.Routing.RouteTable.Routes.Clear();
+            System.Web.Routing.RouteTable.Routes.Ignore("Something");
+
+            var data = tab.GetData(context) as IList<RouteModel>;
+
+            Assert.NotNull(data);
+            Assert.Equal(System.Web.Routing.RouteTable.Routes.Count, data.Count);
+        }
+
+        [Theory, AutoMock]
+        public void SubscribeToConstraintMessageTypes(Routes tab, ITabSetupContext setupContext)
+        { 
+            tab.Setup(setupContext);
+
+            setupContext.MessageBroker.Verify(mb => mb.Subscribe(It.IsAny<Action<RouteBase.ProcessConstraint.Message>>()));
+            setupContext.MessageBroker.Verify(mb => mb.Subscribe(It.IsAny<Action<RouteBase.GetRouteData.Message>>())); 
+        }
+
+        [Theory, AutoMock]
+        public void MatchConstraintMessageToRoute(Routes tab, ITabContext context, System.Web.Routing.IRouteConstraint constraint)
+        {
+            var route = new System.Web.Routing.Route("url", new System.Web.Routing.RouteValueDictionary { { "Test", "Other" } }, new System.Web.Routing.RouteValueDictionary { { "Test", constraint } }, new System.Web.Routing.RouteValueDictionary { { "Data", "Tokens" } }, new System.Web.Routing.PageRouteHandler("~/Path"));
+
+            System.Web.Routing.RouteTable.Routes.Clear();
+            System.Web.Routing.RouteTable.Routes.Add(route); 
+
+            var routeMessage = new RouteBase.GetRouteData.Message(route.GetHashCode(), new System.Web.Routing.RouteData(), "routeName")
+                .AsSourceMessage(route.GetType(), null)
+                .AsTimedMessage(new TimerResult { Duration = TimeSpan.FromMilliseconds(19) });
+            var constraintMessage = new RouteBase.ProcessConstraint.Message(new RouteBase.ProcessConstraint.Arguments(new object[] { (HttpContextBase)null, constraint, "test", (System.Web.Routing.RouteValueDictionary)null, System.Web.Routing.RouteDirection.IncomingRequest }), route.GetHashCode(), true)
+                .AsTimedMessage(new TimerResult { Duration = TimeSpan.FromMilliseconds(25) })
+                .AsSourceMessage(route.GetType(), null);
+
+            context.TabStore.Setup(mb => mb.Contains(typeof(IList<RouteBase.ProcessConstraint.Message>).AssemblyQualifiedName)).Returns(true).Verifiable();
+            context.TabStore.Setup(mb => mb.Contains(typeof(IList<RouteBase.GetRouteData.Message>).AssemblyQualifiedName)).Returns(true).Verifiable();
+
+            context.TabStore.Setup(mb => mb.Get(typeof(IList<RouteBase.ProcessConstraint.Message>).AssemblyQualifiedName)).Returns(new List<RouteBase.ProcessConstraint.Message> { constraintMessage }).Verifiable();
+            context.TabStore.Setup(mb => mb.Get(typeof(IList<RouteBase.GetRouteData.Message>).AssemblyQualifiedName)).Returns(new List<RouteBase.GetRouteData.Message> { routeMessage }).Verifiable();
+             
+            var model = tab.GetData(context) as List<RouteModel>;       
+            var itemModel = model[0];
+             
+            Assert.NotNull(model);
+            Assert.Equal(1, model.Count);
+            Assert.NotNull(itemModel.Constraints);
+            Assert.True(itemModel.IsMatch);
+            Assert.Equal("Test", ((List<RouteConstraintModel>)itemModel.Constraints)[0].ParameterName);
+            Assert.Equal(true, ((List<RouteConstraintModel>)itemModel.Constraints)[0].IsMatch);
+            Assert.NotNull(itemModel.DataTokens);
+            Assert.Equal("Tokens", itemModel.DataTokens["Data"]);
+            Assert.NotNull(itemModel.RouteData);
+            Assert.Equal("Other", ((List<RouteDataItemModel>)itemModel.RouteData)[0].DefaultValue);
+        }
+    }
+}
