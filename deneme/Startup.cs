b8431da767ca5162fd89835c98211670312ae707
cicimen <egemengulkilik@gmail.com>
2013-11-06 05:03:40 +0200

    z

---
+++ deneme/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(deneme.Startup))]
+namespace deneme
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
